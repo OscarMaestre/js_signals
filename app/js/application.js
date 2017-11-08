86f24b0a4b7e70a45c32bb99b795ef8bdeb2e2d5
None <nada@no.com>
2017-11-08 11:21:44 +0100

    Mejorado rendimiento

--- app/js/application.js
+++ app/js/application.js
@@ -1,11 +1,15 @@
 
 var registro;
-var escenario;
-
+var lienzo;
+var ctx;
+var anchura_lienzo, altura_lienzo;
+var x_nuevo_origen , y_nuevo_origen ;
+    
 function dibujar_circulo(x, y, radio, contexto){
     
     contexto.beginPath();
-    
+    x=x-radio;
+    y=y-radio;
     contexto.arc(x, y, radio, 0, 2 * Math.PI, false);
         
     contexto.lineWidth = 1;
@@ -23,22 +27,18 @@ function dibujar_onda(){
     var frecuencia  =   $("#amplitud").val();
     var fase        =   $("#fase").val();
     
+    ctx.save();
     
-    var c = document.getElementById("lienzo");
-    var ctx = c.getContext("2d");
-    ctx.save()
-    var anchura_lienzo  =   c.width;
-    var altura_lienzo   =   c.height ;
+    
+    
     registro.append("Anchura:"+anchura_lienzo+" altura:"+altura_lienzo+"\n");
-    var x_nuevo_origen  =   anchura_lienzo/4;
-    var y_nuevo_origen  =   altura_lienzo/2;
     
     ctx.clearRect(0, 0, anchura_lienzo, altura_lienzo);
     
-    ctx.translate(x_nuevo_origen, y_nuevo_origen);
-    
+        
     dibujar_circulo ( x_nuevo_origen, y_nuevo_origen, amplitud*8, ctx);
-    ctx.restore()
+    ctx.restore();
+    
     
 } //Fin de dibujar seno
 
@@ -47,7 +47,14 @@ function inicio(){
     $("#frecuencia").change(dibujar_onda);
     $("#fase").change(dibujar_onda);
     registro=$("#log");
-    registro.append("Inicio\n")
-    
+    registro.append("Inicio\n");
+    lienzo = document.getElementById("lienzo");
+    ctx = lienzo.getContext("2d");
+    anchura_lienzo  =   lienzo.width;
+    altura_lienzo   =   lienzo.height ;
+    x_nuevo_origen  =   anchura_lienzo/4;
+    y_nuevo_origen  =   altura_lienzo/2;
+    ctx.translate(x_nuevo_origen, y_nuevo_origen);
+    registro.append("Empezmos\n");
 }
 $(document).ready(inicio);
